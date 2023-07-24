08233acd7faa4b6e196a6f5ff13945777c9a3696
jj2084jj <whdms3368@gmail.com>
2023-07-24 23:50:12 +0900

    add / mobile menu

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import NavigationContainer from "@/components/Navigation";
+import NavigationContainer from "@/components/NavigationContainer";
 import "@/styles/globals.css";
 import "@/styles/init.css";
 import type { AppProps } from "next/app";
